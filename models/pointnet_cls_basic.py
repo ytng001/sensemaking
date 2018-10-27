ad95df39d80419c6f5569f6737d29c8a00a79763
ytng001 <35515449+ytng001@users.noreply.github.com>
2018-10-27 09:15:46 +0800

    rbf

--- models/pointnet_cls_basic.py
+++ models/pointnet_cls_basic.py
@@ -8,8 +8,8 @@ sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, '../utils'))
 import tf_util
 #from transform_Vector import input_transform_Vector, inputvectorFeature
-from Transform_RBF_Feature import input_rbfTransform,feature_transform_net,input_transform_net
-
+#from Transform_RBF_Feature import input_rbfTransform,feature_transform_net,input_transform_net
+from RBF_FC import input_rbfTransform
 def placeholder_inputs(batch_size, num_point):
     pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
     labels_pl = tf.placeholder(tf.int32, shape=(batch_size))
@@ -24,52 +24,9 @@ def get_model(point_cloud, is_training, bn_decay=None):
 #    input_image = tf.expand_dims(point_cloud, -1)
     
     with tf.variable_scope('transform_net1') as sc:
-        transform = input_transform_net(point_cloud, is_training, bn_decay)
+        net = input_rbfTransform(point_cloud, is_training, bn_decay)
     
-    netTransformed = tf.matmul(point_cloud, transform)
-    print ("netTransformed ", netTransformed)
-    with tf.variable_scope('RBF_Layer') as sc:
-        net = input_rbfTransform(netTransformed, is_training, bn_decay)
-        
    
-    net = tf.reshape(net, [batch_size, num_point, 1, -1])
-
-    print ("rbf output ", net)
-    net = tf_util.conv2d(net, 64, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv1', bn_decay=bn_decay)
-    
-    
-    with tf.variable_scope('transform_net2') as sc:
-        transform = feature_transform_net(net, is_training, bn_decay, K=64)
-    end_points['transform'] = transform
-    
-    netFeature = tf.matmul(tf.squeeze(net, axis=[2]), transform)
-    netFeature = tf.expand_dims(netFeature, [2])   
-    
-    net = tf_util.conv2d(netFeature, 128, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv3', bn_decay=bn_decay)
-   
-    net = tf_util.conv2d(net, 128, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv4', bn_decay=bn_decay)
-        
-        
-    net = tf_util.conv2d(net, 1024, [1,1],
-                         padding='VALID', stride=[1,1],
-                         bn=True, is_training=is_training,
-                         scope='conv5', bn_decay=bn_decay)
-
-    # Symmetric function: max pooling
-    net = tf_util.max_pool2d(net, [num_point,1],
-                             padding='VALID', scope='maxpool')
-    
-    # MLP on global point cloud vector
-    net = tf.reshape(net, [batch_size, -1])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
                                   scope='fc1', bn_decay=bn_decay)
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
@@ -81,22 +38,13 @@ def get_model(point_cloud, is_training, bn_decay=None):
     return net, end_points
 
 
-def get_loss(pred, label, end_points, reg_weight=0.001):
+def get_loss(pred, label, end_points):
     """ pred: B*NUM_CLASSES,
         label: B, """
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=pred, labels=label)
     classify_loss = tf.reduce_mean(loss)
     tf.summary.scalar('classify loss', classify_loss)
-
-    # Enforce the transformation as orthogonal matrix
-    transform = end_points['transform'] # BxKxK
-    K = transform.get_shape()[1].value
-    mat_diff = tf.matmul(transform, tf.transpose(transform, perm=[0,2,1]))
-    mat_diff -= tf.constant(np.eye(K), dtype=tf.float32)
-    mat_diff_loss = tf.nn.l2_loss(mat_diff) 
-    tf.summary.scalar('mat loss', mat_diff_loss)
-
-    return classify_loss + mat_diff_loss * reg_weight
+    return classify_loss
 
 
 if __name__=='__main__':
