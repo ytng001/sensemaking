3908e5206e63e7eab3fc04e9a489fce6944b7722
ytng001 <35515449+ytng001@users.noreply.github.com>
2018-10-27 19:25:33 +0800

    increase drop out layer

--- models/pointnet_cls_basic.py
+++ models/pointnet_cls_basic.py
@@ -29,9 +29,13 @@ def get_model(point_cloud, is_training, bn_decay=None):
    
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training,
                                   scope='fc1', bn_decay=bn_decay)
+    
+    net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training,
+                          scope='dp1')
+    
     net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training,
                                   scope='fc2', bn_decay=bn_decay)
-    net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
+    net = tf_util.dropout(net, keep_prob=0.4, is_training=is_training,
                           scope='dp1')
     net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
 
