97eb9a826d45bd368819b6667de4201771d8bff5
ytng001 <35515449+ytng001@users.noreply.github.com>
2018-11-01 23:52:03 +0800

    commited

--- models/RBF_RadialNet.py
+++ models/RBF_RadialNet.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(BASE_DIR, '../utils'))
 import tf_util
 from sklearn.metrics.pairwise import rbf_kernel
 
-clusters = 9 #odd number * 3
+clusters = 7 #odd number * 3
 steps = 1/clusters
 
 #define cluster centrod
@@ -46,7 +46,7 @@ def input_rbfTransform(point_cloud, is_training, bn_decay=None, K=3):
         
         print ("input_reshape ", exp_Input)
         print ("tensor ", exp_Clusters)
-        sigma =0.75
+        sigma =1
         distanceSquare = tf.reduce_sum(tf.squared_difference(exp_Input, exp_Clusters),2)
         rbfInput = tf.exp(-distanceSquare / (2* sigma)) #assuming sigma is 1.0
 
